320c0105e0652e29039654a60a927aa18b121143
Dmitry2489 <iva.klicunov2489@mail.ru>
2022-04-18 11:33:50 +0300

    Create Decimal Numbers with JavaScript

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//  Create Decimal Numbers with JavaScript
+
+const ourDecimal = 5.7;
+
+// Only change code below this line
+
+const myDecimal = 5.7
+
+//  Create Decimal Numbers with JavaScript End
