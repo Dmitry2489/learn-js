73674d56c3bb63445cca6f125b30484666764879
Dmitry2489 <iva.klicunov2489@mail.ru>
2022-04-18 12:03:47 +0300

    Create Decimal Numbers with JavaScript

--- js/main.js
+++ js/main.js
@@ -7,3 +7,11 @@ const ourDecimal = 5.7;
 const myDecimal = 5.7
 
 //  Create Decimal Numbers with JavaScript End
+
+// Prevent Infinite Loops with a Valid Terminal Condition
+function myFunc() {
+    for (let i = 1; i <= 4; i += 2) {
+      console.log("Still going!");
+    }
+  }
+// Prevent Infinite Loops with a Valid Terminal Condition End
