0eee40bf007148926407fc7dec8b987bf286c0e0
Dmitry2489 <iva.klicunov2489@mail.ru>
2022-04-19 09:53:12 +0300

    Use Caution When Reinitializing Variables Inside a Loop

--- js/main.js
+++ js/main.js
@@ -23,3 +23,27 @@ const product = 2.0 * 2.5;
 // Divide One Decimal by Another with JavaScript
 const quotient = 4.4 / 2; // Change this line
 // Divide One Decimal by Another with JavaScript End
+
+
+// Use Caution When Reinitializing Variables Inside a Loop
+function zeroArray(m, n) {
+    // Creates a 2-D array with m rows and n columns of zeroes
+    let newArray = [];
+    for (let i = 0; i < m; i++) {
+      let row = [];
+      // Adds the m-th row into newArray
+  
+      for (let j = 0; j < n; j++) {
+        // Pushes n zeroes into the current row to create the columns
+        row.push(0);
+      }
+      // Pushes the current row, which now has n zeroes in it, to the array
+      newArray.push(row);
+    }
+    return newArray;
+  }
+  
+  let matrix = zeroArray(3, 2);
+  console.log(matrix);
+
+  // Use Caution When Reinitializing Variables Inside a Loop End
