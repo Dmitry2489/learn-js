235fc34162d0a16ecb044c260207bdc094bb3430
Dmitry2489 <iva.klicunov2489@mail.ru>
2022-04-22 12:47:48 +0300

    Understand String Immutability

--- js/main.js
+++ js/main.js
@@ -144,3 +144,14 @@ const lastName = "Lovelace";
 // Only change code below this line
 firstLetterOfLastName = lastName[0]; // Change this line
 // Use Bracket Notation to Find the First Character in a String End
+
+// Understand String Immutability
+
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+// Understand String Immutability End
